2977f8a262b34b43a0005ab88a8ac50d9ba2fd3f
Erik Timmers <e.timmers@gmail.com>
2019-12-28 13:58:58 +0100

    WIP

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd openapi-generator
-mvn clean install
-java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate -i ../openapi.yaml  -g elm -o ../test
-cd ../test
-elm make src/**/*.elm
